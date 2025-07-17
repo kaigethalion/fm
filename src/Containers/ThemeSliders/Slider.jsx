c86bdd29c5b940dcb38ca1e959663af511dc76dc
Kaige Chapman <kaigechapman@icloud.com>
2025-07-16 19:41:37 -0700

    adding ffm logo picture

--- src/Containers/ThemeSliders/Slider.jsx
+++ src/Containers/ThemeSliders/Slider.jsx
@@ -1,31 +1,42 @@
-import React from 'react';
+import React from "react";
 
 const Slider = () => {
-    return (
-        <div className="theme_slider_1">
-            <div className="slider">
-                <div className="container">
-                    <div className="row">
-                        <div className="col-lg-6">
-                            <div className="slide_content">
-                                <h5 className="sub_heading">CALL US ANYTIME<span> we at yo momma house</span></h5>
-                                <h2 className="heading">Freedom Forest Mangement</h2>
-                                <p className="desc"> Where You Find The Best Builders Safe, Reliable and Cost Effective Construction company. We offer best construction Services.</p>
-                                <div className="slider_button">
-                                    <a href="/about" className="button">Cash Me Outside</a>
-                                </div>
-                            </div>
-                        </div>
-                        <div className="col-lg-5 d-flex align-items-end">
-                            <div className="layer_object">
-                                <img src="" alt="" />
-                            </div>
-                        </div>
-                    </div>
+  return (
+    <div className="theme_slider_1">
+      <div className="slider">
+        <div className="container">
+          <div className="row">
+            <div className="col-lg-6">
+              <div className="slide_content">
+                <h5 className="sub_heading">
+                  CALL US ANYTIME<span> we at yo momma house</span>
+                </h5>
+                <h2 className="heading">Freedom Forest Mangement</h2>
+                <p className="desc">
+                  {" "}
+                  Where You Find The Best Builders Safe, Reliable and Cost
+                  Effective Construction company. We offer best construction
+                  Services.
+                </p>
+
+                <div className="slider_button">
+                  <a href="/about" className="button">
+                    Cash Me Outside
+                  </a>
+                </div>
+              </div>
+            </div>
+            <div className="col-lg-5 d-flex align-items-end">
+              <div className="layer_object"></div>
+                <div style={{ position: "absolute", zIndex: 10 }}>
+                  <img src="/img/Logo_ffm.png" alt="" />
                 </div>
             </div>
+          </div>
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default Slider;
